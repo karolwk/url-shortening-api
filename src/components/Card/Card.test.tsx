4f2b7bca4310f55a323285d938f466e3b40e7498
karolwk <krl@o2.pl>
2022-07-03 22:00:39 +0200

    added test for the icon

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -1,12 +1,13 @@
-import { render, screen } from '../../shared/test-utils';
+import { render, screen, TestIcon } from '../../shared/test-utils';
 import '@testing-library/jest-dom/extend-expect';
 import Card from './Card';
 
 test('should render properly', () => {
-  const pages = ['Test1', 'Test2', 'Test3'];
-
-  render(<Card icon={<></>} title="Test Title" content="Test Content" />);
+  render(
+    <Card icon={<TestIcon />} title="Test Title" content="Test Content" />
+  );
 
   expect(screen.getByText(/Test Title/)).toBeInTheDocument();
   expect(screen.getByText(/Test Content/)).toBeInTheDocument();
+  expect(screen.getByLabelText('test-icon')).toBeInTheDocument();
 });
