04c141411004debaf40c117d1007efb7dc6efb22
karolwk <krl@o2.pl>
2022-07-09 19:46:07 +0200

    added user events to tests

--- src/components/ShortUrlForm/ShortUrlForm.test.tsx
+++ src/components/ShortUrlForm/ShortUrlForm.test.tsx
@@ -1,4 +1,10 @@
-import { render, screen, handlers } from '../../shared/test-utils';
+import {
+  render,
+  screen,
+  handlers,
+  fireEvent,
+  waitFor,
+} from '../../shared/test-utils';
 import '@testing-library/jest-dom/extend-expect';
 import { setupServer } from 'msw/node';
 import ShortUrlForm from './ShortUrlForm';
@@ -19,4 +25,15 @@ test('should render properly', () => {
   expect(screen.getByText(/Shorten it!/)).toBeInTheDocument();
   expect(screen.getByRole('textbox')).toBeInTheDocument();
   expect(screen.getByRole('button')).toBeInTheDocument();
+});
+test('user should get three results of shortening API', async () => {
+  render(<ShortUrlForm />);
+  const input = screen.getByRole('textbox');
+  const button = screen.getByRole('button', { name: /Shorten it!/ });
+
+  fireEvent.input(input, { target: { value: 'test.pl/test/' } });
+  fireEvent.click(button);
+  await waitFor(() => screen.findByText(/Copy/));
+  //@ts-ignore
+  screen.debug();
 });
